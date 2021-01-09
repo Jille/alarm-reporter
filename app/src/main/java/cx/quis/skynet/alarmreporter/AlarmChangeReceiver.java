292aa6ac817afe665b3df043ac75bd868a12d0dd
Jille Timmermans <jille@quis.cx>
2021-01-09 17:02:34 +0000

    Empty project

---
+++ app/src/main/java/cx/quis/skynet/alarmreporter/AlarmChangeReceiver.java
@@ -0,0 +1,15 @@
+package cx.quis.skynet.alarmreporter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmChangeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
